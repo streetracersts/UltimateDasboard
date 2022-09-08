e66744db6e8eacf277f060fe707f861de54c2a8b
Ricardo <streetracersts@hotmail.com>
2022-09-08 20:45:08 -0300

    Hide toolbars

--- app/src/main/java/com/rissi/ultimatedashboard/MainActivity.kt
+++ app/src/main/java/com/rissi/ultimatedashboard/MainActivity.kt
@@ -4,6 +4,9 @@ package com.rissi.ultimatedashboard
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
@@ -19,5 +22,15 @@ class MainActivity : AppCompatActivity() {
          velocidade.text = 88.toString()
         val dataAtual = findViewById<TextView>(R.id.txtData)
         dataAtual.text = formatted
+        hideSystemBars();
     }
+    private fun hideSystemBars() {
+        val windowInsetsController = ViewCompat.getWindowInsetsController(window.decorView) ?: return
+         // Configure the behavior of the hidden system bars
+         windowInsetsController.systemBarsBehavior =
+         WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+         // Hide both the status bar and the navigation bar
+         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+    }
+
 }
